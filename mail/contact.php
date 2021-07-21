620ef062c0f862e19e51aefd132c1c450557c26c
TechinGrow <86523497+techingrow@users.noreply.github.com>
2021-07-21 13:47:42 +0530

    Update contact.php

--- mail/contact.php
+++ mail/contact.php
@@ -9,7 +9,7 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "techgrow911@gmail.com"; // Change this email to your //
+$to = "techingrow@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
@@ -17,4 +17,11 @@ $header .= "Reply-To: $email";
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
+  
+if($email!=NULL)
+{
+  mail($to,$subject,$text,$headers);
+}
+header("Location:www.shivinsurance.live");
+
 ?>
